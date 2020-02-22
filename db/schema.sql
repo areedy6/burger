786e6d3f85f0f70086eacafebcefdeec77518cff
andrewreedy <reedyandrew11@gmail.com>
2020-02-22 12:09:55 -0500

    heroku

--- db/schema.sql
+++ db/schema.sql
@@ -5,8 +5,5 @@
 -- -- Create the table burgers.
 -- CREATE TABLE burgers
 -- (
---     id INT NOT NULL AUTO_INCREMENT,
---     burger_name VARCHAR(50) NOT NULL,
---     devoured BOOLEAN DEFAULT false,
---     PRIMARY KEY (id)
+--   
 -- );
