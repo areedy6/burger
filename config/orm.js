ab3f95e7b2e548c252e0858ac498ea7483f403b1
andrewreedy <reedyandrew11@gmail.com>
2020-02-22 09:48:20 -0500

    more cleaning

--- config/orm.js
+++ config/orm.js
@@ -1,7 +1,7 @@
-// Import (require) connection.js
+
 var connection = require("../config/connection.js");
 
-// Helper function for SQL syntax.
+
 function printQuestionMarks(num) {
     var arr = [];
     for (var i = 0; i < num; i++) {
@@ -10,29 +10,27 @@ function printQuestionMarks(num) {
     return arr.toString();
 }
 
-// Helper function to convert object key/value pairs to SQL syntax
 function objToSql(ob) {
     var arr = [];
-    // loop through the keys and push the key/value as a string int arr
+    
     for (var key in ob) {
         var value = ob[key];
-        // check to skip hidden properties
+        
         if (Object.hasOwnProperty.call(ob, key)) {
-            // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+          
             if (typeof value === "string" && value.indexOf(" ") >= 0) {
                 value = "'" + value + "'";
             }
-            // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
-            // e.g. {sleepy: true} => ["sleepy=true"]
+            
             arr.push(key + "=" + value);
         }
     }
-    // translate array of strings to a single comma-separated string
+    
     return arr.toString();
 }
 
 var orm = {
-    // Display all burgers in the db.
+ 
     selectAll: function(table, cb) {
         var queryString = "SELECT * FROM " + table + ";";
 
@@ -43,7 +41,7 @@ var orm = {
             cb(result);
         });
     },
-    // Add a burger to the db.
+    
     insertOne: function(table, cols, vals, cb) {
         var queryString = "INSERT INTO " + table;
         queryString += " (";
@@ -62,7 +60,7 @@ var orm = {
             cb(result);
         });
     },
-    // Set burger devoured status to true.
+ 
     updateOne: function(table, objColVals, condition, cb) {
         var queryString = "UPDATE " + table;
         queryString += " SET ";
@@ -79,7 +77,7 @@ var orm = {
             cb(result);
         });
     },
-    // Delete a burger from the db.
+    
     deleteOne: function(table, condition, cb) {
         var queryString = "DELETE FROM " + table;
         queryString += " WHERE ";
@@ -96,5 +94,5 @@ var orm = {
     }
 };
 
-// Export the ORM object in module.exports.
+
 module.exports = orm;
